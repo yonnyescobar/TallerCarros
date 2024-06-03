28e39d8611d06a6e46ffb6372109c5b1250a6d0b
yonnyescobar@gmail.com <yonnyescobar@gmail.com>
2024-06-03 09:48:02 -0500

    Modified functions in visita.js

--- Web/Paginas/Scripts/Visita.js
+++ Web/Paginas/Scripts/Visita.js
@@ -38,3 +38,41 @@ async function LlenarClientes() {
         $("#dvMensaje").html(error);
     }
 }
+
+async function LlenarVehiculo() {
+    try {
+        const Respuesta = await fetch("http://localhost:50046/api/Vehiculos",
+            {
+                method: "GET",
+                mode: "cors",
+                headers: { "Content-Type": "application/json" }
+            });
+        const Resultado = await Respuesta.json();
+        for (i = 0; i < Resultado.length; i++) {
+            $("#cboVehiculo").append('<option value="' + Resultado[i].id_vehiculo + '">' + Resultado[i].placa + '</option >');
+        }
+
+    }
+    catch (error) {
+        $("#dvMensaje").html(error);
+    }
+}
+
+async function LlenarEmpleado() {
+    try {
+        const Respuesta = await fetch("http://localhost:50046/api/Empleados",
+            {
+                method: "GET",
+                mode: "cors",
+                headers: { "Content-Type": "application/json" }
+            });
+        const Resultado = await Respuesta.json();
+        for (i = 0; i < Resultado.length; i++) {
+            $("#cboEmpleado").append('<option value="' + Resultado[i].id_empleado + '">' + Resultado[i].nombre + " " + Resultado[i].apellido + '</option >');
+        }
+
+    }
+    catch (error) {
+        $("#dvMensaje").html(error);
+    }
+}
