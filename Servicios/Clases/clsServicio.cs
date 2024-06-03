aef9646e83d8dfd066a5c7485b1c04553cf96805
yonnyescobar@gmail.com <yonnyescobar@gmail.com>
2024-06-03 09:06:21 -0500

    Created the class and controller for services with your functions and methods

---
+++ Servicios/Clases/clsServicio.cs
@@ -0,0 +1,23 @@
+﻿using Servicios.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Servicios.Clases
+{
+	public class clsServicio
+	{
+		private TallerCarrosEntities dbTallerCarros = new TallerCarrosEntities();
+
+		public Servicio servicio { get; set; }
+
+		public List<Servicio> ConsultarServicios()
+		{
+			return dbTallerCarros.Servicios
+				.Where(s => s.id_servicio != 0)
+				.OrderBy(s => s.nombre) 
+				.ToList();
+		}
+	}
+}
---
+++ Servicios/Controllers/ServiciosController.cs
@@ -0,0 +1,23 @@
+﻿using Servicios.Clases;
+using Servicios.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace Servicios.Controllers
+{
+	[EnableCors(origins: "http://localhost:50167", headers: "*", methods: "*")]
+	public class ServiciosController : ApiController
+	{
+		// GET Servicios
+		public List<Servicio> Get()
+		{
+			clsServicio _servicio = new clsServicio();
+			return _servicio.ConsultarServicios();
+		}
+	}
+}
