ab5f56361af6468e53fc9a3fff21a1f8f0486a2a
yonnyescobar@gmail.com <yonnyescobar@gmail.com>
2024-06-02 22:09:24 -0500

    Created format json for visit

---
+++ Web/Paginas/Scripts/Visita.js
@@ -0,0 +1,40 @@
+﻿jQuery(function () {
+
+    $("#dvMenu").load("../Paginas/Menu.html");
+
+    $("#btnInsertar").on("click", function () {
+        EjecutarComando("POST");
+    });
+    $("#btnActualizar").on("click", function () {
+        EjecutarComando("PUT");
+    });
+    $("#btnEliminar").on("click", function () {
+        EjecutarComando("DELETE");
+    });
+    $("#btnConsultar").on("click", function () {
+        Consultar();
+    });
+    LlenarClientes();
+    LlenarVehiculo();
+    LlenarEmpleado();
+    LlenarTabla();
+});
+
+async function LlenarClientes() {
+    try {
+        const Respuesta = await fetch("http://localhost:50046/api/Clientes",
+            {
+                method: "GET",
+                mode: "cors",
+                headers: { "Content-Type": "application/json" }
+            });
+        const Resultado = await Respuesta.json();
+        for (i = 0; i < Resultado.length; i++) {
+            $("#cboCliente").append('<option value="' + Resultado[i].id_cliente + '">' + Resultado[i].nombre + " " + Resultado[i].apellido + '</option >');
+        }
+
+    }
+    catch (error) {
+        $("#dvMensaje").html(error);
+    }
+}
