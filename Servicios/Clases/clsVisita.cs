f0a463b278e083669bb0960bb446232db1299106
yonnyescobar@gmail.com <yonnyescobar@gmail.com>
2024-06-03 10:07:48 -0500

    Created new data model, new database and modified the class visit

--- Servicios/Clases/clsVisita.cs
+++ Servicios/Clases/clsVisita.cs
@@ -18,7 +18,7 @@ namespace Servicios.Clases
 			{
 				dbTallerCarros.Visitas.Add(visita);
 				dbTallerCarros.SaveChanges();
-				return "Se agendó cita para el: " + visita.fecha + " a las " + visita.hora + "correctamente";
+				return "Se agendó cita para el: " + visita.fecha + "correctamente";
 			}
 			catch (Exception ex)
 			{
@@ -29,25 +29,17 @@ namespace Servicios.Clases
 		public IQueryable ConsultarVisitas()
 		{
 			return from Vi in dbTallerCarros.Set<Visita>()
-				   join Cl in dbTallerCarros.Set<Cliente>()
-				   on Vi.id_cliente equals Cl.id_cliente
-				   join Ve in dbTallerCarros.Set<Vehiculo>()
-				   on Vi.id_vehiculo equals Ve.id_vehiculo
-				   join Mv in dbTallerCarros.Set<MarcasVehiculo>()
-				   on Ve.id_marca equals Mv.id_marca
-				   join Em in dbTallerCarros.Set<Empleado>()
-				   on Vi.id_empleado equals Em.id_empleado
 				   join Se in dbTallerCarros.Set<Servicio>()
 				   on Vi.id_servicio equals Se.id_servicio
 				   orderby Vi.fecha ascending
 				   select new
 				   {
-					   Cliente = Cl.nombre,
-					   Vehiculo = Mv.nombre,
-					   Servicio = Se.nombre,
-					   Empleado = Em.nombre + " " + Em.apellido,
-					   Fecha = Vi.fecha,
-					   Hora = Vi.hora
+					   Documento = Vi.documento,
+					   Nombre = Vi.nombre,
+					   Apellido = Vi.apellido,
+					   Telefono = Vi.telefono,
+					   Correo = Vi.email,
+					   Fecha = Vi.fecha
 				   };
 		}
 	}
